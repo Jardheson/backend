d6cd00bbe5f5bc3088a112ea41b4f5102c32e46a
Jardheson <herlles2022@gmail.com>
2026-03-03 15:27:05 -0300

    Tarefa: atualizar o submódulo do backend para o estado "sujo"
    O submódulo foi marcado como "sujo" devido a alterações não confirmadas no diretório de trabalho. Esta atualização reflete o estado atual do submódulo do backend.

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
   coverageDirectory: 'coverage',
   collectCoverageFrom: [
     'src/**/*.js',
-    '!src/server.js', // Exclude entry point
-    '!src/config/**', // Exclude config
+    '!src/server.js', 
+    '!src/config/**', 
   ],
 };
